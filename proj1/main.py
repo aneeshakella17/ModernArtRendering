ae3b737a8abdc9be0393dd9cd85ac0c4bf2805c7
mac <mac@macs-MacBook-Pro-2.local>
2018-01-31 18:37:35 -0800

    Trial 1

--- proj1/main.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-# import colorize_skel;
-#
-# exec(open("colorize_skel.py").read());
--- proj3/main.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python
-import hybrid_image_starter
-exec(open("hybrid_image_starter").read());
